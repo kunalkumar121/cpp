90888ba5164fd8bde4767b699a234046f53a6c39
kunal kumar <152509346+kunalkumar121@users.noreply.github.com>
2025-01-01 22:02:05 +0530

    Add files via upload

---
+++ hello.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace  std;
+
+int main ()
+{
+    cout << "hello world c++" << endl;
+    int a = 4;
+    int b = 7;
+    cout << a*b << endl;
+    
+
+    return 0;
+}

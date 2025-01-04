2ce7bd8d88b28d05b8c396d4f4779697c86bc4e7
kunal kumar <152509346+kunalkumar121@users.noreply.github.com>
2025-01-04 21:31:10 +0530

    Add files via upload

---
+++ sum.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+
+int main () {
+    int a;
+    int b;
+
+    //enter a
+    cout<< "enter a :";
+    cin>> a;
+
+    //enter b
+
+    cout << "enter b : ";
+    cin>>b;
+
+    //calcuate
+    int sum = a+b;
+    cout <<" sum = " << sum << endl;
+
+    return 0;
+}
